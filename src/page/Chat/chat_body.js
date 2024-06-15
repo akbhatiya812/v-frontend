2b77392ca0f437636530ada6c5cd48734c7f3a0e
akbhatiya812 <161412219+akbhatiya812@users.noreply.github.com>
2024-06-15 16:57:23 +0530

    Update chat_body.js

--- src/page/Chat/chat_body.js
+++ src/page/Chat/chat_body.js
@@ -34,7 +34,7 @@ function ChatBody() {
         console.log("jere")
         new Notification("Chat Application", {
           body: "New Message received",
-          icon: 'https://www.pinclipart.com/picdir/big/369-3699390_notification-png-notification-icon-png-free-clipart.png' // Optional: Add an icon
+          icon: 'https://webstockreview.net/images/notification-icon-png-5.png' // Optional: Add an icon
         });
       }
       setUsemsg((prevMessages) => [
